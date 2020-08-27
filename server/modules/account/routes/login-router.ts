b1656306d5c9f2ebc25b7222e9d29d67d064f191
gumiranda <imartytk@hotmail.com>
2020-08-27 18:52:23 -0300

    codigo zika

--- server/modules/account/routes/login-router.ts
+++ server/modules/account/routes/login-router.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import { makeSignUpController } from '../../../bin/patterns/factories/signup-factory';
+import { makeSignUpController } from '../factories/signup-factory';
 import { adaptRoute } from '../../../bin/configuration/adapters/express-route-adapter';
-import { makeLoginController } from '../../../bin/patterns/factories/login-factory';
+import { makeLoginController } from '../factories/login-factory';
 const router = Router();
 router.post('/signup', adaptRoute(makeSignUpController()));
 router.post('/login', adaptRoute(makeLoginController()));
